f72fb74e83eb1805ecf2b7c701bc3d864208d20e
Premsai Varma <premsaivarmacoding@gmail.com>
2023-03-28 17:10:41 +0530

    added 04 webpack

---
+++ WebXR/04-webpack/src/script.js
@@ -0,0 +1,26 @@
+import './style.css'
+import * as THREE from 'three'
+
+// scene 
+const scene = new THREE.Scene()
+
+// cube 
+const geometry = new THREE.BoxGeometry(1,1,1)
+const material = new THREE.MeshBasicMaterial({color:'red'})
+const mesh = new THREE.Mesh(geometry,material)
+scene.add(mesh)
+
+let sizes = {
+    width:800,
+    height:600
+}
+// camera 
+const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height)
+camera.position.z = 2
+scene.add(camera)
+const canvas = document.querySelector('.webgl')
+const renderer = new THREE.WebGL1Renderer({
+    canvas: canvas 
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
