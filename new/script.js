93d1b2cbf1403260ca6dc1545a15e674003a1891
Premsai Vamra <premsaivarmacoding@gmail.com>
2023-04-06 20:40:54 +0530

    script.js file is added

---
+++ new/script.js
@@ -0,0 +1,41 @@
+import * as THREE from "https://cdn.skypack.dev/three@0.130.1/build/three.module.js";
+import { OrbitControls } from "https://cdn.skypack.dev/three@0.130.1/examples/jsm/controls/OrbitControls.js";
+import {ARButton} from "https://cdn.skypack.dev/three@0.130.1/examples/jsm/webxr/ARButton.js"
+const canvas = document.querySelector(".webxr");
+// Scene
+const scene = new THREE.Scene()
+
+// Cube 
+const box = new THREE.BoxGeometry(1,1,1)
+const material = new THREE.MeshBasicMaterial({color:0x0000ff})
+const mesh = new THREE.Mesh(box, material)
+scene.add(mesh) 
+//sizes 
+const sizes = {
+    width: innerWidth,
+    height: innerHeight
+}
+
+// Camera 
+const camera = new THREE.PerspectiveCamera(75, sizes.width/sizes.height)
+camera.position.z = 5;
+scene.add(camera)
+
+const controls = new OrbitControls(camera, canvas)
+// renderer
+const renderer = new THREE.WebGLRenderer(
+    {
+        canvas:canvas
+    }
+)
+renderer.setSize(sizes.width, sizes.height)
+renderer.render(scene, camera)
+
+const tick = () => {
+    renderer.render(scene,camera)
+    window.requestAnimationFrame(tick)
+}
+tick()
+
+const button = ARButton.createButton(renderer)
+document.body.appendChild(button)
