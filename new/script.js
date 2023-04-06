bdf86313fa98e287c8295ea4117ef0cd8573dabe
Premsai Vamra <premsaivarmacoding@gmail.com>
2023-04-06 21:17:12 +0530

    added some changes 1 in ARButton

--- new/script.js
+++ new/script.js
@@ -1,16 +1,19 @@
 import * as THREE from "https://cdn.skypack.dev/three@0.130.1/build/three.module.js";
 import { OrbitControls } from "https://cdn.skypack.dev/three@0.130.1/examples/jsm/controls/OrbitControls.js";
 import {ARButton} from "https://cdn.skypack.dev/three@0.130.1/examples/jsm/webxr/ARButton.js"
+
 const canvas = document.querySelector(".webxr");
+
 // Scene
 const scene = new THREE.Scene()
 
 // Cube 
 const box = new THREE.BoxGeometry(1,1,1)
-const material = new THREE.MeshBasicMaterial({color:0x0000ff})
+const material = new THREE.MeshBasicMaterial({color:0x00ff00})
 const mesh = new THREE.Mesh(box, material)
 scene.add(mesh) 
-//sizes 
+
+// Sizes 
 const sizes = {
     width: innerWidth,
     height: innerHeight
@@ -22,14 +25,20 @@ camera.position.z = 5;
 scene.add(camera)
 
 const controls = new OrbitControls(camera, canvas)
-// renderer
+
+// Renderer
 const renderer = new THREE.WebGLRenderer(
     {
         canvas:canvas
     }
 )
 renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+
+// Enable XR
+renderer.xr.enabled = true;
+
+document.body.appendChild(renderer.domElement)
+
 controls.enableDamping = true 
 const clock = new THREE.Clock()
 const tick = () => {
@@ -39,9 +48,10 @@ const tick = () => {
     controls.update()
     renderer.render(scene,camera)
     window.requestAnimationFrame(tick)
-    renderer.xr.enabled = true 
 }
+
 tick()
+
 console.log("Hello World")
 
 const button = ARButton.createButton(renderer);
